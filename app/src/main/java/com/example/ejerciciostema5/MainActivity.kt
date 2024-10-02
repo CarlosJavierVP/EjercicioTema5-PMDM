2d9e3193e329fb0e70aa9b7339e64798c884ab5b
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-02 13:42:53 +0200

    commit calcular edad

--- app/src/main/java/com/example/ejerciciostema5/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciostema5/MainActivity.kt
@@ -32,8 +32,16 @@ class MainActivity : AppCompatActivity(){
         var day = c.get(Calendar.DAY_OF_MONTH)
         var month = c.get(Calendar.MONTH)+1
         var year = c.get(Calendar.YEAR)
+        val t = findViewById<TextView>(R.id.textView2)
+
+
         var dateShow = DatePickerDialog(this,
-            {datePicker, i, i1, i2 -> textDate.text = "Naciste el $i2/${i1+1}/$i" }, year, month, day)
+            {
+            datePicker, i, i1, i2 -> textDate.text = "Naciste el $i2/${i1+1}/$i"
+                var ar = calculaDias(month,i1+1,day,i,year,i2)
+                t.text = getString(R.string.calcular, ar[0],ar[1])
+
+            }, year, month, day)
 
 
         val today = findViewById<TextView>(R.id.textView)
@@ -44,15 +52,79 @@ class MainActivity : AppCompatActivity(){
             dateShow.show()
         }
 
-        val text = getString(R.string.calcular, 20,210)
 
-        val t = findViewById<TextView>(R.id.textView2)
-
-        t.text = text
 
 
     }
 
+
+    private fun calculaDias(
+        mesActual: Int,
+        mesNacimiento: Int,
+        diaActual: Int,
+        diaNacimiento: Int,
+        anhoActual: Int,
+        anhoNacimiento: Int
+    ): IntArray {
+        var dias = 0
+        val calculo = IntArray(2)
+        var anhos = anhoActual - anhoNacimiento
+        if (mesActual >= mesNacimiento) {
+            for (i in mesNacimiento + 1..mesActual) dias += numeroDeDias(i, anhoActual)
+            if (diaActual < diaNacimiento && mesActual == mesNacimiento) {
+                anhos--
+                dias = 365 + diaActual - diaNacimiento
+            } else {
+                dias += diaActual - diaNacimiento
+            }
+        } else {
+            anhos--
+            dias = diasRestantes(diaNacimiento, mesNacimiento, anhoActual - 1)
+            val veces = 12 + mesActual - mesNacimiento - 1
+            var mesVer = mesNacimiento + 1
+            var anhoVer = anhoActual - 1
+            for (i in 1..veces) {
+                if (mesVer > 12) {
+                    mesVer = 1
+                    anhoVer++
+                }
+                dias += numeroDeDias(mesVer, anhoVer)
+                mesVer++
+            }
+            dias += diaActual
+        }
+        calculo[0] = anhos
+        calculo[1] = dias
+        return calculo
+    }
+
+    private fun esBisiesto(anho: Int): Boolean {
+        var bisiesto = false
+        if (anho > 1582) {
+            if (anho % 400 == 0 || anho % 4 == 0 && anho % 100 != 0) {
+                bisiesto = true
+            }
+        } else {
+            bisiesto = false
+        }
+        return bisiesto
+    }
+
+    private fun numeroDeDias(m: Int, a: Int): Int {
+        return when (m) {
+            1, 3, 5, 7, 8, 10, 12 -> 31
+            4, 6, 9, 11 -> 30
+            else -> if (esBisiesto(a)) {
+                29
+            } else {
+                28
+            }
+        }
+    }
+
+    private fun diasRestantes(dia: Int, mes: Int, anho: Int): Int {
+        return numeroDeDias(mes, anho) - dia
+    }
 
 
 
